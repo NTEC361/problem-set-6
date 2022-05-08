34aee44a3d34a6edbd5d4d6d940854e48c5166d1
360mtucker <46805953+360mtucker@users.noreply.github.com>
2022-05-08 11:31:21 -0700

    Add files via upload

---
+++ problem-set-6/problem-set-6-problem-7.py
@@ -0,0 +1,83 @@
+# NTEC 365
+# YOUR NAME
+# Spring 2022
+# problem-set-6-problem-7.py
+
+"""
+- There are 20 items in this assignment.
+- DO NOT DELETE OR REMOVE ANY OF THE COMMENTS BELOW
+- DO NOT DELETE OR REMOVE ANY OF THE COMMENTS BELOW
+- DO NOT DELETE OR REMOVE ANY OF THE COMMENTS BELOW
+- DO NOT DELETE OR REMOVE ANY OF THE COMMENTS BELOW
+
+😀 Yes I repeated that line on purpose...
+"""
+
+# Make sure you changed Line 1 to be your first and last name (1/2 point)
+
+
+teacher = "Bruce Elgort"
+# Using string slicing print the first letters "Bruc" from the teacher variable (1/2 point)
+
+
+# Using string slicing print out the last 2 letters from the teacher variable (1/2 point)
+
+
+# Using string slicing print out the last 8 letters from the teacher variable (1/2 point)
+
+
+# Write a for loop that displays each character from the teacher variable on a separate line (1/2 point)
+
+
+# Using the .count() string method, display the number of u's in the teacher variable and print it out (1/2 point)
+
+
+# Using the .count() string method, display the number of z's in the teacher variable and print it out (1/2 point)
+
+
+# Using the .find() string method, find the location of the letter 'E' in the teacher variable and print it out (1/2 point)
+
+
+# Replace the word 'Bruce' in the teacher variable with the word 'Lawrence' and print it out (1/2 point)
+
+
+rockGroup = "alice cooper"
+
+# Using the .title() string method and the rockGroup variable, print out the variable so it shows in title case (1/2 point)
+
+
+# Using the .upper() string method and the rockGroup variable, print out the variable so it shows all letters in upper case (1/2 point)
+
+
+# Using the rockGroup variable, print out every other letter starting with the first letter 'a' (1/2 point)
+
+
+# Using a for loop, print out each letter of the variable rockGroup starting with the last letter and going to the first letter (1/2 point)
+
+
+grades = [100, 100, 0, 0, 0, 55, 95, 96, 88, 100, 77, 87]
+
+# Print out the length of the grades list using the approrpriate Python function learned in this class (1/2 point)
+
+
+# Write a for loop that iterates through the grades list and prints each list element on a new line (1/2 point)
+
+
+# Using a for loop, the accumulator pattern and the grades list, calculate the avarage grade and print it out (1/2 point)
+
+
+# Append the number 99 to the end of the list (1/2 point)
+
+
+aRandomSentence = "I like turtles especially green ones"
+
+# Using the aRandomSentence variable, use the .split() string method to create a list called sentenceWords (1/2 point)
+
+
+# Using the sentenceWords variable and a loop, print out the length of each word. You will first need to split the sentence into a list (1/2 point)
+
+
+# Write a for loop (counted loop) to print out each of the words in the sentenceWords list (1/2 point)
+
+
+# Save your work, stage, commit and push it to GitHub
